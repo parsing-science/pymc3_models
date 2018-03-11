735b911fb08fc4c657fbdeb7751f73618bad0d8d
Rémi Louf <remi@sounds.am>
2018-03-11 16:55:16 +0100

    Amend style in code and docstrings

--- pymc3_models/models/NaiveBayes.py
+++ pymc3_models/models/NaiveBayes.py
@@ -8,58 +8,65 @@ import theano
 
 from pymc3_models.exc import PyMC3ModelsError
 from pymc3_models.models import BayesianModel
+from pymc3_models.utils import normalize
 
-
-__all__ = ["GaussianNaiveBayes"]
-
-
-# Gaussian Naive Bayes
-# -----------------------------------------------------------------------------
 
 class GaussianNaiveBayes(BayesianModel):
-    """ Naive Bayes classification built using PyMC3.
+    """
+    Naive Bayes classification built using PyMC3.
 
     The Gaussian Naive Bayes algorithm assumes that the random variables
     that describe each class and each feature are independent and distributed
-    according to independent Normal distributions.
+    according to Normal distributions.
+
+    Example
+    -------
+    >>> import pymc3_models as pmo
+    >>> 
+    >>> model = pmo.GaussianNaiveBayes()
+    >>> model.fit(X,y)
+    >>> model.predict_proba(X)
+    >>> model.predict(X)
 
     See the documentation of the `create_model` method for details on the model
-    itself.  
+    itself.
     """
 
     def __init__(self):
         super(GaussianNaiveBayes, self).__init__()
 
     def create_model(self):
-        """ Creates and returns the PyMC3 model.
+        """
+        Creates and returns the PyMC3 model.
 
         We note :math:`x_{jc}` the value of the j-th element of the data vector :math:`x`
-        conditionned on x belonging to the class :math:`c`. The Gaussian Naive Bayes 
+        conditioned on x belonging to the class :math:`c`. The Gaussian Naive Bayes
         algorithm models :math:`x_{jc}` as:
-        
+
         .. math::
 
             x_{jc} \\sim Normal(\\mu_{jc}, \\sigma_{jc})
-        
+
         While the probability that :math:`x` belongs to the class :math:`c` is given by the
         categorical distribution:
-        
+
         .. math::
 
             P(y=c|x_i) = Cat(\\pi_1, \dots, \\pi_C)
 
         where :math:`\pi_i` is the probability that a vector belongs to category :math:`i`.
-        
+
         We assume that the :math:`\pi_i` follow a Dirichlet distribution:
-        
+
         .. math::
-            
+
             \\pi \\sim Dirichlet(\\alpha)
 
-        with hyperparameter :math:`\\alpha = [1, .., 1]`. The :math:`\\mu_{jc}` are sampled from
-        a Normal distribution centred on :math:`0` with variance :math:`100`, and the :math:`\\sigma_{jc}`
-        are sampled from a HalfNormal distribuion of variance :math:`100`:
-        
+        with hyperparameter :math:`\\alpha = [1, .., 1]`. The :math:`\\mu_{jc}`
+        are sampled from a Normal distribution centred on :math:`0` with
+        variance :math:`100`, and the :math:`\\sigma_{jc}` are sampled from a
+        HalfNormal distribuion of variance :math:`100`:
+
         .. math::
 
             \\mu_{jc} \\sim Normal(0, 100)
@@ -72,68 +79,67 @@ class GaussianNaiveBayes(BayesianModel):
         Returns
         -------
         A PyMC3 model
-        
+
         References
         ----------
-        [1]: Murphy, K. P. (2012). Machine learning: a probabilistic perspective.
+        .. [1] Murphy, K. P. (2012). Machine learning: a probabilistic perspective.
         """
-        
+
         # The data
-        X = theano.shared(np.zeros((self.n_samples, self.n_features)))
-        y = theano.shared(np.zeros(self.n_samples))
+        X = theano.shared(np.zeros((self.num_training_samples, self.num_pred)))
+        y = theano.shared(np.zeros(self.num_training_samples, dtype=int))
 
         self.shared_vars = {
             'model_input': X,
             'model_output': y
         }
-        
+
         model = pm.Model()
         with model:
             # Priors
-            alpha = np.ones(self.n_classes)
-            pi = pm.Dirichlet('pi', alpha, shape=self.n_classes)
-            mu = pm.Normal('mu', 0, 100, shape=(self.n_classes, self.n_features))
-            sigma = pm.HalfNormal('sigma', 100, shape=(self.n_classes, self.n_features))
+            alpha = np.ones(self.num_cats)
+            pi = pm.Dirichlet('pi', alpha, shape=self.num_cats)
+            mu = pm.eormal('mu', mu=0, sd=100, shape=(self.num_cats, self.num_pred))
+            sigma = pm.HalfNormal('sigma', 100, shape=(self.num_cats, self.num_pred))
 
             # Assign classes to data points
-            z = pm.Categorical('z', pi, shape=self.n_samples, observed=y)
+            z = pm.Categorical('z', pi, shape=self.num_training_samples, observed=y)
 
-            # The components are independant and normally distributed
-            xi = pm.Normal('xi', mu[z], sigma[z], observed=X)
+            # The components are independent and normally distributed
+            xi = pm.Normal('xi', mu=mu[z], sd=sigma[z], observed=X)
 
         return model
 
     def fit(self, X, y, inference_type='advi', minibatch_size=None, inference_args=None):
-        """ Train the Naive Bayes model.
+        """
+        Train the Naive Bayes model.
 
         Parameters
         ----------
-        X : numpy array of shape [n_samples, n_features]. Contains the data
-            points.
+        X : numpy array, shape [num_training_samples, num_pred]. Contains the data points.
 
-        y : numpy array of  shape [n_samples,]. Contains the category of the data
-            points.
+        y : numpy array, shape [num_training_samples,]. Contains the category of the data points.
 
         inference_type : string, specifies which inference method to call.
-            Default is 'advi'. Currently, only 'advi' and 'nuts'are implemented.
+            Default is 'advi'. Currently, only 'advi' and 'nuts' are implemented.
 
         minibatch_size : int, number of samples to include in each minibatch
             for ADVI. Defaults to None so minibatch is not run by default.
 
         inference_args : dict, arguments to be passed to the inference methods.
             Check the PyMC3 documentation.
-        
+
         Returns
         -------
-        The current instance of the model.
+        The current instance of the GaussianNaiveBayes class.
         """
-        self.n_samples, self.n_features = X.shape 
-        self.n_classes = len(np.unique(y))
+        self.num_training_samples, self.num_pred = X.shape
+        self.num_cats = len(np.unique(y))
         self.inference_type = inference_type
 
         if not inference_args:
             inference_args = self._set_default_inference_args()
-        
+
         if not self.cached_model:
             self.cached_model = self.create_model()
 
@@ -151,82 +157,100 @@ class GaussianNaiveBayes(BayesianModel):
         self._inference(inference_type, inference_args)
 
         return self
-    
+
     def predict_proba(self, X):
-        """ Predicts the probabilities that the data points belong to each
-        category.
+        """
+        Predicts the probabilities that the data points belong to each category.
 
         Given a new data point :math:`\\vec{x}`, we want to estimate the probability that
         it belongs to a category :math:`c`. Following the notations in [1], the probability
         reads:
-        
-        .. math::
-
-            P(y=c|\\vec{x}, \\mathcal{D}) = P(y=c|\\mathcal{D}) \\prod_{j=1}^{n_{dims}} P(x_j|y=c, \\mathcal{D})
-
-        We previously used the data :math:`\\mathcal{D}` to estimate the distribution of the
-        parameters :math:`\\vec{\\mu}`, :math:`\\vec{\\pi}` and :math:`\\vec{\\sigma}`. To compute the above
-        probability, we need to integrate over the values of these parameters:
 
         .. math::
 
-            P(y=c|\\vec{x}, \\mathcal{D}) = \\left[\\int Cat(y=c|\\vec{\\pi})P(\\vec{\\pi}|\\mathcal{D})\\mathrm{d}\\vec{\\pi}\\right] \\
-                                        \\int P(\\vec{x}|\\vec{\\mu}, \\vec{\\sigma})P(\\vec{\\mu}|\\mathcal{D})P(\\vec{\\sigma}|\\mathcal{D})\\
-                                        \\mathrm{d}\\vec{\\mu}\\mathrm{d}\\vec{\\sigma}
-        
+            P(y=c|\\vec{x}, \\mathcal{D}) = P(y=c|\\mathcal{D}) \\prod_{j=1}^{n_{dims}} \\
+                                            P(x_j|y=c, \\mathcal{D})
+
+        We previously used the data :math:`\\mathcal{D}` to estimate the
+        distribution of the parameters :math:`\\vec{\\mu}`, :math:`\\vec{\\pi}`
+        and :math:`\\vec{\\sigma}`. To compute the above probability, we need
+        to integrate over the values of these parameters:
+
+        .. math::
+
+            P(y=c|\\vec{x}, \\mathcal{D}) = \\left[\\int Cat(y=c|\\vec{\\pi})P(\\vec{\\pi}|\\
+                                            \\mathcal{D})\\mathrm{d}\\vec{\\pi}\\right]
+                                            \\int P(\\vec{x}|\\vec{\\mu}, \\vec{\\sigma})\\
+                                            P(\\vec{\\mu}|\\mathcal{D})\\
+                                            P(\\vec{\\sigma}|\\mathcal{D})\\
+                                            \\mathrm{d}\\vec{\\mu}\\mathrm{d}\\vec{\\sigma}
+
         Parameters
         ----------
-        X : numpy array of shape [n_samples, n_features]. Contains the points
+        X : numpy array, shape [num_training_samples, num_pred]. Contains the points
             for which we want to predict the class
 
         Returns
         -------
-        A numpy array of shape [n_samples, n_classes] that contains the probabilities
+        A numpy array of shape [num_training_samples, num_cats] that contains the probabilities
         that each sample belong to each category.
 
-        [1]: Murphy, K. P. (2012). Machine learning: a probabilistic perspective.
+        References
+        ----------
+        .. [1] Murphy, K. P. (2012). Machine learning: a probabilistic perspective.
         """
- 
+
         if self.trace is None:
             raise PyMC3ModelsError("Run fit() on the model before predict()")
 
-        predictions = np.array([])
+        posterior_prediction = np.array([])
         for x in X:
             prob_per_sample = scipy.stats.norm(self.trace['mu'], self.trace['sigma']).pdf(x)
-            prob_per_feature = [np.sum(prob_per_sample[:,:,i], axis=0)/len(self.trace['mu']) for i in range(self.n_features)]
+            prob_per_feature = [
+                    np.sum(prob_per_sample[:, :, i], axis=0)/len(self.trace['mu'])
+                        for i in range(self.num_pred)
+            ]
             prob_per_class = normalize(ft.reduce(lambda x, y: x*y, prob_per_feature))
-            if len(predictions) == 0:
-                predictions = prob_per_class
+            if len(posterior_prediction) == 0:
+                posterior_prediction = prob_per_class
             else:
-                predictions = np.vstack((predictions, prob_per_class))
+                posterior_prediction = np.vstack((posterior_prediction, prob_per_class))
 
-        return predictions
+        return posterior_prediction
 
     def predict(self, X):
-        """ Classify new data with a trained Naive Bayes model.
+        """
+        Classify new data with a trained Naive Bayes model. The output is the point
+        estimate of the posterior predictive distribution that corresponds to the
+        one-hot loss function.
 
         Parameters
         ----------
-        X : numpy array of shape [n_samples, n_features] that contains the data
+        X : numpy array, shape [num_training_samples, num_pred]. Contains the data
             to classify.
 
         Returns
         -------
-        A numpy array of shape [n_samples,] that contains the predicted class to
+        A numpy array of shape [num_training_samples,] that contains the predicted class to
         which the data points belong.
         """
         proba = self.predict_proba(X)
-        pred = np.argmax(proba, axis=1)
-        return pred
+        predictions = np.argmax(proba, axis=1)
+        return predictions
 
     def score(self, X, y):
-        """ Scores new data with a trained model.
+        """
+        Scores new data with a trained model.
 
         Parameters
         ----------
-        X : numpy array, shape [n_samples, n_features]. Contains the data points.
+        X : numpy array, shape [num_training_samples, num_pred]. Contains the data points.
 
-        y : numpy array, shape [n_samples,]. Contains the category of the data points.
+        y : numpy array, shape [num_training_samples,]. Contains the category of the data points.
+
+        Returns
+        -------
+        A float representing the accuracy score of the predictions.
         """
 
         return accuracy_score(y, self.predict(X))
@@ -234,33 +258,16 @@ class GaussianNaiveBayes(BayesianModel):
     def save(self, file_prefix):
         params = {
             'inference_type': self.inference_type,
-            'num_cats': self.n_classes,
-            'num_pred': self.n_features,
-            'num_training_samples': n_samples
+            'num_cats': self.num_cats,
+            'num_pred': self.num_pred,
+            'num_training_samples': self.num_training_samples
         }
         super(GaussianNaiveBayes, self).save(file_prefix, params)
-    
+
     def load(self, file_profile):
-        params = super(GaussianNaiveBayes, self).load(file_prefix, load_custom_params=True)
-        
+        params = super(GaussianNaiveBayes, self).load(file_profile, load_custom_params=True)
+
         self.inference_type = params['inference_type']
         self.num_cats = params['num_cats']
         self.num_pred = params['num_pred']
         self.num_training_samples = params['num_training_samples']
-
-
-# Functions
-# -----------------------------------------------------------------------------
-
-def normalize(array):
-    """ Normalize values in the array to get probabilities.
-
-    Parameter
-    --------
-    array : numpy array of shape [1,]
-
-    Returns
-    -------
-    A normalized array
-    """
-    return array/np.sum(array)
--- tests/models/test_NaiveBayes.py
+++ tests/models/test_NaiveBayes.py
@@ -12,64 +12,65 @@ from pymc3_models.exc import PyMC3ModelsError
 
 class GaussianNaiveBayesTestCase(unittest.TestCase):
     def setUp(self):
-        """ Set up a test case with synthetic data.
+        """
+        Set up a test case with synthetic data.
         """
 
         self.num_cats = 3
-        self.num_features = 10
+        self.num_pred = 10
         self.num_samples = 100000
-        
+
         # Generate data
         ## Priors
         alpha = np.ones(self.num_cats)
         pi = np.random.dirichlet(alpha)
-        mu = np.random.normal(0, 100, size=(self.num_cats, self.num_features))
-        sigma = scipy.stats.halfnorm(loc=0, scale=100).rvs(size=(self.num_cats, self.num_features))
+        mu = np.random.normal(0, 100, size=(self.num_cats, self.num_pred))
+        sigma = scipy.stats.halfnorm(loc=0, scale=100).rvs(size=(self.num_cats, self.num_pred))
         ## Data
         Y = np.random.choice(range(self.num_cats), self.num_samples, p=pi)
         x_vectors = []
         for i in Y:
             x_vectors.append(np.random.normal(mu[i], sigma[i]))
         X = np.vstack(x_vectors)
-        
-        
+
         # Split into train/test sets
         split = train_test_split(X, Y, test_size=0.4)
-        self.X_train = split[0] 
-        self.X_test= split[1]
+        self.X_train = split[0]
+        self.X_test = split[1]
         self.Y_train = split[2]
         self.Y_test = split[3]
 
         self.test_GNB = GaussianNaiveBayes()
         self.test_dir = tempfile.mkdtemp()
-        
+
         # Fit the model once and for all
         self.test_GNB.fit(self.X_train, self.Y_train, minibatch_size=2000)
 
     def tearDown(self):
-        """ Tear down the testing environment.
+        """
+        Tear down the testing environment.
         """
         shutil.rmtree(self.test_dir)
 
 
-# Test fit
 class GaussianNaiveBayesFitTestCase(GaussianNaiveBayesTestCase):
     def test_fit_returns_correct_model(self):
-        """ Test the model initialization and fit
+        """
+        Test the model initialization and fit
 
-        Currently, only the sign of infered parameters is checked
+        Currently, only the sign of inferred parameters is checked
         against the sign of the parameters used to generate the data.
 
-        This is, however, not ideal. It is necessary to find better 
+        This is, however, not ideal. It is necessary to find better
         strategies to test probabilistic code.
         """
 
         print('')
-        
+
         # Check that the model correctly infers dimensions
         self.assertEqual(self.num_cats, self.test_GNB.num_classes)
         self.assertEqual(self.num_samples, self.test_GNB.num_samples)
-        self.assertEqual(self.num_features, self.test_GNB.num_features)
+        self.assertEqual(self.num_pred, self.test_GNB.num_pred)
 
         # TODO: How do you write tests for a stochastic mode?
         # TODO: Diagnose the sampling with a reasonable sampling size?
@@ -91,21 +92,19 @@ class GaussianNaiveBayesPredictProbaTest(GaussianNaiveBayesTestCase):
         self.assertEqual(probs.shape, self.Y_test.shape)
         self.assertEqual(stds.shape, self.Y_test.shape)
 
-
     def test_predict_proba_raises_error_if_not_fit(self):
-        """ Test that an error is raised when you try to make predictions before fitting the model.
-        """
         with self.assertRaises(PyMC3ModelsError) as no_fit_error:
             test_GNB = GaussianNaiveBayes()
             test_GNB.predict_proba(self.X_train, self.Y_train)
 
-        expected = 'Run fit on the model before predict' # precarious, define a new Exception type
+        expected = 'Run fit on the model before predict'  # precarious, define a new Exception type
         self.assertEqual(str(no_fit_error.exception), expected)
 
 
 class GaussianNaiveBayesPredictionTestCase(GaussianNaiveBayesTestCase):
     def test_predict_returns_predictions(self):
-        """ Test that the predict() function's  output has the correct shape.
+        """
+        Test that the predict() function's  output has the correct shape.
         """
         print('')
         preds = self.test_GNB.predict(self.X_test)
@@ -118,10 +117,11 @@ class GaussianNaiveBayesScoreTestCase(GaussianNaiveBayesTestCase):
         score = self.test_GNB.score(self.X_test, self.Y_test)
         # What to test for?
 
+
 class GaussianNaiveBayesSaveAndLoadTestCase(GaussianNaiveBayesTestCase):
     def test_save_and_load_work_correctly(self):
         print('')
-        probs1 = self.test_GNB.predict_proba(self.X_test)
+        probs = self.test_GNB.predict_proba(self.X_test)
         self.test_GNB.save(self.test_dir)
 
         GNB2 = GaussianNaiveBayes()
